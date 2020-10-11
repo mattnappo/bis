5eb1ae98ddf1326bcd33aab1eedb3826e0feab8e
xoreo <mattnappo@gmail.com>
2020-10-11 00:07:19 -0400

    Wrote some of the status bar.

--- src/main.c
+++ src/main.c
@@ -8,30 +8,20 @@
 #define MAX_BUFF 2048
 #define MAX_FNAME 32
 
+#define CENTER(item) ((COLS / 2) - (strlen(item) / 2))
+#define END(item) (COLS - strlen(item))
+
 enum mode {
     INSERT,
     REPLACE,
     COMMAND
 };
 
-char *stringify_mode(enum mode mode)
-{
-    switch (mode) {
-
-    case INSERT:
-        return "INSERT";
-
-    case REPLACE:
-        return "REPLACE";
-
-    case COMMAND:
-        return "COMMAND";
-
-    default:
-        printf("error, unknown mode\n");
-        return NULL;
-    }
-}
+char str_modes[][10] = {
+    "INSERT",
+    "REPLACE",
+    "COMMAND"
+};
 
 typedef struct point {
     uint16_t x;
@@ -54,6 +44,7 @@ struct bis_state *init_bis_state()
     struct bis_state *state = malloc(sizeof(struct bis_state));
     state->buffer = calloc(MAX_BUFF, 1);
     state->filename = calloc(MAX_FNAME, 1);
+    state->filename = "newfile";
     state->mode = INSERT; // TODO: Start in command mode
     state->cx = 0;
     state->cy = 0;
@@ -67,7 +58,12 @@ void destroy_bis_state(struct bis_state *state)
     free(state);
 }
 
-void init()
+void sync_cursor(struct bis_state *state)
+{
+    move(state->cx, state->cy);
+}
+
+void init_bis_scr()
 {
     initscr();
     cbreak();
@@ -81,38 +77,51 @@ void init()
     */
     clear();
     start_color();
+    init_pair(1, COLOR_YELLOW, COLOR_GREEN);
     keypad(stdscr, TRUE);
 }
 
 void draw_bottom_bar(struct bis_state *state)
 {
-    init_pair(1, COLOR_YELLOW, COLOR_GREEN);
+    // Print window mode
+    char mode_str[20];
+    // TODO: Color this
+    sprintf(mode_str, "%s", str_modes[state->mode]);
+    mvprintw(LINES - 1, 0, mode_str);
+
+    // Print cursor pos
+    char pos[25];
+    sprintf(pos, "%d:%d", state->cy, state->cx);
+    mvprintw(LINES - 1, CENTER(pos), pos);
+    
+    // Print file name
+    char fname[MAX_FNAME + 5];
+    sprintf(fname, "%s", state->filename);
+
+    // int index = (CENTER(pos) - strlen(mode_str) / 2) / 2;
+    int index = strlen(mode_str) + 2;
+    mvprintw(LINES - 1, index, fname);
+
+    // Print window size
+    char size[30];
+    sprintf(size, "%dx%d", LINES, COLS);
+    mvprintw(LINES - 1, END(size), size);
+
+    sync_cursor(state);
 }
 
 int main(void)
 {
-    init();
-
     struct bis_state *core = init_bis_state();
-   
-    char lines[10], cols[10];
 
-    sprintf(lines, "%d", LINES);
-    sprintf(cols, "%d", COLS);
-
-    mvprintw(0, 0, lines);
-    mvprintw(1, 0, cols);
-    refresh();
-    sleep(3);
-
+    init_bis_scr();
+     
     draw_bottom_bar(core);
-
-    sleep(3);
+    refresh();
 
     // if user types :wq, quit
-    sleep(1);
+    sleep(5);
     endwin();
-
     destroy_bis_state(core);
 
     return 0;
